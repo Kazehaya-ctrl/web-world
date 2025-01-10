219c12a1d0133e3435ba38b6b22bf74aee1ae770
Kazehaya-ctrl <mahitomaki96@gmail.com>
2025-01-10 19:03:44 +0530

    upt: Game Functions

--- src/components/game/gameFunction.ts
+++ src/components/game/gameFunction.ts
@@ -1,7 +1,23 @@
-function preload() {}
+import Phaser from "phaser";
 
-function create() {}
+var cursors: any;
+var player: any
 
-function update() {}
+function preload(this: Phaser.Scene) {
+  this.load.image("background", "../../assets/sky.png");
+  this.load.image("player", "../../assets/star.png")
+}
+
+function create(this: Phaser.Scene) {
+  this.add.image(Math.random() * 10, Math.random() * 10, "player")
+  player = Phaser.GameObjects.Sprite()
+  cursors = this.input.keyboard?.createCursorKeys()
+}
+
+function update(this: Phaser.Scene) {
+  if (cursors.left.isDown) {
+
+  }
+}
 
 export { preload, create, update };
--- src/utils/gameConfig.ts
+++ src/utils/gameConfig.ts
@@ -1,7 +1,7 @@
 import Phaser from "phaser";
 import { preload, create, update } from "../components/game/gameFunction";
 
-var gameConfig = {
+var gameConfig: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
